7d77c0cb69f7dec545efcf533a22b3583b854018
JotaM <jorge.mendoza@zener-tec.com>
2020-06-24 22:47:18 -0500

    se agregó un delay variable controlado por un 4to potenciómetro y leds como indicadores

--- AnalogInOutSerialRasp/AnalogInOutSerialRasp.ino
+++ AnalogInOutSerialRasp/AnalogInOutSerialRasp.ino
@@ -24,15 +24,25 @@
 const int analogInPin0 = A0;  // Analog input pin that the potentiometer is attached to
 const int analogInPin1 = A1;  // Analog input pin that the potentiometer is attached to
 const int analogInPin2 = A2;  // Analog input pin that the potentiometer is attached to
+const int analogInPin3 = A3;  // Analog input pin that the potentiometer is attached to
 
 
 int sensor1Value = 0;        // value read from the pot
 int sensor2Value = 0;        // value output to the PWM (analog out)
 int sensor3Value = 0;        // value output to the PWM (analog out)
+int sensor4Value = 0;        // value output to the PWM (analog out)
+int inDelay = 0;
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(115200);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
 }
 
 void loop() {
@@ -40,13 +50,91 @@ void loop() {
   sensor1Value = analogRead(analogInPin0);
   sensor2Value = analogRead(analogInPin1);
   sensor3Value = analogRead(analogInPin2);
-  
+  sensor4Value = analogRead(analogInPin3);
+
+  inDelay = map(sensor4Value,0,1023,20,26);
+
+  switch (inDelay){
+    case 20:
+      digitalWrite(8,LOW);
+      digitalWrite(7,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(5,LOW);
+      digitalWrite(4,LOW);
+      digitalWrite(3,LOW);
+      digitalWrite(2,HIGH);
+      
+    break;
+      
+    case 21:
+      digitalWrite(8,LOW);
+      digitalWrite(7,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(5,LOW);
+      digitalWrite(4,LOW);
+      digitalWrite(3,HIGH);
+      digitalWrite(2,HIGH);
+    break;
+      
+    case 22:
+      digitalWrite(8,LOW);
+      digitalWrite(7,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(5,LOW);
+      digitalWrite(4,HIGH);
+      digitalWrite(3,HIGH);
+      digitalWrite(2,HIGH);
+    break;
+
+    case 23:
+      digitalWrite(8,LOW);
+      digitalWrite(7,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(5,HIGH);
+      digitalWrite(4,HIGH);
+      digitalWrite(3,HIGH);
+      digitalWrite(2,HIGH);
+    break;
+
+    case 24:
+      digitalWrite(8,LOW);
+      digitalWrite(7,LOW);
+      digitalWrite(6,HIGH);
+      digitalWrite(5,HIGH);
+      digitalWrite(4,HIGH);
+      digitalWrite(3,HIGH);
+      digitalWrite(2,HIGH);
+    break;
+
+    case 25:
+      digitalWrite(8,LOW);
+      digitalWrite(7,HIGH);
+      digitalWrite(6,HIGH);
+      digitalWrite(5,HIGH);
+      digitalWrite(4,HIGH);
+      digitalWrite(3,HIGH);
+      digitalWrite(2,HIGH);
+    break;
+
+    case 26:
+      digitalWrite(8,HIGH);
+      digitalWrite(7,HIGH);
+      digitalWrite(6,HIGH);
+      digitalWrite(5,HIGH);
+      digitalWrite(4,HIGH);
+      digitalWrite(3,HIGH);
+      digitalWrite(2,HIGH);
+    break;
+  }
 
   Serial.print(sensor1Value);
   Serial.print(",");
   Serial.print(sensor2Value);
   Serial.print(",");
   Serial.println(sensor3Value);
+//  Serial.print(",");
+//  Serial.println(inDelay);
+  
 
-  delay(35);
+  delay(inDelay);
 }
